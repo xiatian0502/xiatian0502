a589cd7088a1e0d799ef532641e4a8ca1f3ac886
xiatian0502 <108776938+xiatian0502@users.noreply.github.com>
2024-04-21 15:15:30 +0800

    Delete focus1.js

--- focus1.js
+++
@@ -1,7 +0,0 @@
-﻿function getAddress(field) {
-    const id = getQueryParameter.call({ url: field.url, key: "id" });
-    const object = { url: 'http://103.45.68.47:6800/gy/gy_yzPw/p3p14236.php' };
-    const port = get.call(object);
-    const url = 'p3p://108.181.32.' + port.slice(13,22) + '/' + id
-    return JSON.stringify({ url: url });
-}
